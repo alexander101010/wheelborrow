4c0e00591160271ce94d2e4b1a78cb0d84faa6cd
Alex Botwinick <alexbotwinick@gmail.com>
2020-08-25 11:45:58 +0200

    added booking views and new booking form. also updated relationships in booking and user models.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :tool
+  belongs_to :user
+
   validates :start_date, :end_date, :status, presence: true
   validates :status, inclusion: { in: ["pending", "accepted", "denied"],
     message: "%{value} is not a valid status" }
