bfc3569b046a5c949012f8d973a8644199425b71
Alex Botwinick <alexbotwinick@gmail.com>
2020-08-27 11:53:44 +0200

    permitted user edit fields so you can actually update your information via dropdown selection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo, :address, :email, :password])
   end
 end
