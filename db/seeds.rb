f26b9bee8d9c04632921f0e7473f5f2310d86375
Marielle de Jong <jong.de.me@gmail.com>
2020-08-27 16:06:33 +0200

    added seeds for user, tool, review and booking model

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,22 @@
 require 'faker'
 require "open-uri"
 
+puts "Start time = #{Time.now}"
 puts "Emptying database"
 
-Tool.destroy_all
 Review.destroy_all
 Booking.destroy_all
+Tool.destroy_all
 User.destroy_all
 
 
-file = URI.open('https://picsum.photos/200/300')
+TOOLS = %w(hammer saw jackhammer paintbrush drill chainsaw bucket broom ladder)
+STATUS = %w(pending accepted denied)
 
 puts "creating new users"
 
 5.times do
+  file = URI.open('https://www.google.com/search?q=tree&oq=tree&aqs=chrome..69i57j0l3j69i65j69i60l3.16088j0j4&sourceid=chrome&ie=UTF-8')
   user = User.new
   user.first_name = Faker::Name.first_name
   user.last_name = Faker::Name.last_name
@@ -31,32 +34,47 @@ puts "creating new users"
   user.address = Faker::Address.full_address
   user.latitude = Faker::Address.latitude
   user.longitude = Faker::Address.longitude
-  file = URI.open(Faker::Fillmurray.image)
+  # file = URI.open(Faker::Fillmurray.image)
   user.photo.attach(io: file, filename: "#{user.first_name.downcase}.jpg", content_type: 'image/jpg')
   user.save!
 
-  # 2.times do
-  #   tool = Tool.new
-  #   tool.name = Faker::Vehicle.make
-  #   tool.description = Faker::Vehicle.standard_specs
-  #   tool.price = rand(1..100)
-  #   tool.category = ["hand tools", "power tools", "gardening tools"].sample
-  #   file = URI.open(Faker::Fillmurray.image)
-  #   tool.photo.attach(io: file, filename: "#{user.first_name.downcase}.jpg", content_type: 'image/jpg')
-  #   tool.user = user
-  #   tool.save!
+  2.times do
+    tool = Tool.new
+    tool.name = TOOLS.sample
+    tool.description = Faker::Vehicle.standard_specs
+    tool.price = rand(1..100)
+    tool.category = ["hand tools", "power tools", "gardening tools"].sample
+    file = URI.open("https://source.unsplash.com/1600x900/?#{tool.name}")
+    tool.photo.attach(io: file, filename: "#{user.first_name.downcase}.jpg", content_type: 'image/jpg')
+    tool.user = user
+    tool.save!
 
-  #   3.times do
-  #     review = Review.new
-  #     review.content = Faker::Relationship.familial
-  #     review.rating = rand(1..5)
-  #   end
-  # end
+    2.times do
+      booking = Booking.new
+      booking.start_date = Faker::Date.between(from: '2020-09-23', to: '2020-09-25')
+      booking.end_date = Faker::Date.between(from: '2020-09-26', to: '2020-09-29')
+      booking.status = STATUS.sample
+      booking.user = user
+      booking.tool = tool
+      booking.save
+
+      2.times do
+        review = Review.new
+        review.content = Faker::Relationship.familial
+        review.rating = rand(1..5)
+        review.tool = tool
+        review.save
+      end
+    end
+  end
 end
 
 puts "#{User.count} users created"
 puts "#{Tool.count} tools created"
 puts "#{Review.count} reviews created"
 puts "#{Booking.count} bookings created"
+
+puts "----------------------------------------"
+puts "End time #{Time.now}"
 
 
