6aaceeb108cf19e58c122882c01b5b85ac92af9e
Marielle de Jong <jong.de.me@gmail.com>
2020-08-27 14:32:06 +0200

    added reviews model with references to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_082512) do
+ActiveRecord::Schema.define(version: 2020_08_27_122829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,15 @@ ActiveRecord::Schema.define(version: 2020_08_27_082512) do
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "tools", force: :cascade do |t|
     t.string "description"
     t.string "category"
@@ -88,5 +97,6 @@ ActiveRecord::Schema.define(version: 2020_08_27_082512) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "tools"
   add_foreign_key "bookings", "users"
+  add_foreign_key "reviews", "users"
   add_foreign_key "tools", "users"
 end
