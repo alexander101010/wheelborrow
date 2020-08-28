de60eac31819f04867b0a1649c841c0de6dc5ff7
Alex Botwinick <alexbotwinick@gmail.com>
2020-08-28 12:24:35 +0200

    added decline/cancel buttons to account lends, should only render if applicable

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  before_action :find, only: [:show, :edit, :destroy]
+  before_action :find, only: [:show, :edit, :destroy, :confirm, :decline]
   def index
     @bookings = Booking.all
   end
@@ -12,18 +12,25 @@ class BookingsController < ApplicationController
   end
 
   def cancel
-    @booking.status = "cancelled"
+    @booking = Booking.find(params[:format])
+    @booking.status = "Cancelled"
+    @booking.save
+    flash[:notice] = "You have cancelled this booking request"
+    # redirect_back(fallback_location: root_path)
   end
 
   def confirm
-    @booking.status = "accepted"
+    @booking.status = "Accepted"
     @booking.save
     flash[:notice] = "You have accepted the pending booking request"
     redirect_back(fallback_location: root_path)
   end
 
   def decline
-    @booking.status = "declined"
+    @booking.status = "Declined"
+    @booking.save
+    flash[:notice] = "You have declined pending booking request"
+    redirect_back(fallback_location: root_path)
   end
 
   def new
@@ -35,7 +42,7 @@ class BookingsController < ApplicationController
     @booking = Booking.new(params_format)
     @booking.tool = Tool.find(params[:tool_id])
     @booking.user = current_user
-    @booking.status = 'pending'
+    @booking.status = 'Pending'
 
     if @booking.save
       redirect_to account_path
@@ -48,7 +55,7 @@ class BookingsController < ApplicationController
 private
 
   def find
-    @booking = booking.find(params[:id])
+    @booking = Booking.find(params[:id])
   end
 
   def params_format
